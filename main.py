ec97c33b924868b85112b42779f0da86d8b0dda7
Amon <amonogino@gmail.com>
2026-04-21 17:54:14 +0300

    debug

--- main.py
+++ main.py
@@ -1,31 +1,22 @@
-from fastapi import  FastAPI
-
+from fastapi import FastAPI
 from pydantic import BaseModel
-
-
-
+from typing import Optional
 
 app = FastAPI()
 
-
-
 class Blog(BaseModel):
-    pass
-    
+    title: str
+    body: str
+    published: Optional[bool] = False
+
 @app.post('/blog')
-def create_blog(requet: Blog):
-    return {}
-    
-
-
+def create_blog(request: Blog):
+    return {"data": f"Blog is created wit title a {request.title}"}
 
 @app.get('/')
 def index():
-    return {'data':{"yo":"F"}}
+    return {'data': {"yo": "F"}}
 
-
-
-
-@app.get('/blog/unpublised')
+@app.get('/blog/unpublished')
 def unpublished():
-    return {"unpublished"}
+    return {"data": "unpublished blogs"}
